eed5ab700540ff82fd6800d9182ed0d191b69aaa
Gary O'Neall <gary@sourceauditor.com>
2021-11-24 12:19:31 -0800

    Add lockfile
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- scaffold.py
+++ scaffold.py
@@ -1,7 +1,6 @@
 # Copyright The Linux Foundation
 # SPDX-License-Identifier: Apache-2.0
 
-import json
 from pathlib import Path
 from operator import itemgetter
 import os
@@ -24,6 +23,9 @@ from delivering import doDelivered
 from metrics import getMetrics, printMetrics
 from metricsfile import saveMetrics
 from transfer import doTransfer
+from datetime import datetime
+
+LOCK_FILE_NAME = "lock.lock"
 
 def printUsage():
     print(f"""
@@ -54,6 +56,7 @@ Commands:
 
   Admin:
     transfer:         Transfer project scans from old Fossology server to new
+    clearlock:        Clear the lock file
 
 """)
 
@@ -92,6 +95,175 @@ def fossdriverSetup(fossdriverrc_path):
 
     return server
 
+def lockfile(config_dir):
+    ''' Lock the lockfile for the month
+    config_dir - directory for the month containing the configuration file
+    Returns true if the month was successfully locked or false if the month is already locked
+    '''
+    
+    lockfile = os.path.join(config_dir, LOCK_FILE_NAME)
+    try:
+        with open(lockfile, 'x') as f:
+            f.write('Open for lock on ')
+            f.write(str(datetime.now()))
+            return True
+    except:
+        return False
+
+def unlockfile(config_dir):
+    ''' Unlocks the lockfile for the month
+    '''
+    lockfile = os.path.join(config_dir, LOCK_FILE_NAME)
+    os.remove(lockfile)
+
+def clear_lock(config_dir):
+    '''
+    Clears the lock file
+    '''
+    lockfile = os.path.join(config_dir, LOCK_FILE_NAME)
+    if os.path.exists(lockfile):
+        os.remove(lockfile)
+
+def exec_command(SCAFFOLD_HOME, cfg, args):
+    '''
+    Executes the command
+    cfg - Configuration
+    args - Arguments - args[1] month; args[2] command; args[3] optional project; args[4] optional subproject
+    returns true if successful, false if not
+    '''
+    # we'll check if added optional args limit to one prj / sp
+    prj_only = ""
+    sp_only = ""
+    ran_command = False
+
+    if len(args) >= 3:
+        command = args[2]
+
+    if len(args) >= 4:
+        prj_only = args[3]
+        if len(args) >= 5:
+            sp_only = args[4]
+
+    if command == "status":
+        ran_command = True
+        status(cfg, prj_only, sp_only)
+
+    elif command == "newmonth":
+        ran_command = True
+        copyToNextMonth(SCAFFOLD_HOME, cfg)
+
+    elif command == "run":
+        ran_command = True
+        saveBackupConfig(SCAFFOLD_HOME, cfg)
+
+        # set up fossdriver server connection
+        fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriverrc.json")
+        fdServer = fossdriverSetup(fossdriverrc_path)
+        if not fdServer:
+            print(f"Unable to connect to Fossology server with fossdriver")
+            sys.exit(1)
+
+        # run commands
+        doNextThing(SCAFFOLD_HOME, cfg, fdServer, prj_only, sp_only)
+
+        # save modified config file
+        saveConfig(SCAFFOLD_HOME, cfg)
+
+    elif command == "ws":
+        ran_command = True
+        if prj_only == "" or sp_only == "":
+            print(f"ws command requires specifying project and subproject")
+            sys.exit(1)
+
+        # run WS agent manually if between ZIPPEDCODE and CLEARED state
+        # does not modify the config file
+        runManualWSAgent(cfg, prj_only, sp_only)
+
+    elif command == "clear":
+        ran_command = True
+        saveBackupConfig(SCAFFOLD_HOME, cfg)
+
+        # clear if in RANAGENTS state
+        doCleared(SCAFFOLD_HOME, cfg, prj_only, sp_only)
+
+        # save config file, even if not modified (b/c saved backup)
+        saveConfig(SCAFFOLD_HOME, cfg)
+
+    elif command == "approve":
+        ran_command = True
+        saveBackupConfig(SCAFFOLD_HOME, cfg)
+
+        # approve if in MADEDRAFTFINDINGS state
+        doApprove(SCAFFOLD_HOME, cfg, prj_only, sp_only)
+
+        # save config file, even if not modified (b/c saved backup)
+        saveConfig(SCAFFOLD_HOME, cfg)
+
+    elif command == "printemail":
+        ran_command = True
+
+        # print email details if in UPLOADEDREPORTS state
+        printEmail(cfg, prj_only, sp_only)
+
+    elif command == "printlinks":
+        ran_command = True
+
+        # print report link details if in UPLOADEDREPORTS state
+        printAllLinks(cfg, prj_only, sp_only)
+
+    elif command == "printreportlinks":
+        ran_command = True
+
+        # print report link details if in UPLOADEDREPORTS state
+        printReportLinks(cfg, prj_only, sp_only)
+
+    elif command == "deliver":
+        ran_command = True
+        saveBackupConfig(SCAFFOLD_HOME, cfg)
+
+        # clear if in UPLOADEDSPDX state
+        doDelivered(SCAFFOLD_HOME, cfg, prj_only, sp_only)
+
+        # save config file, even if not modified (b/c saved backup)
+        saveConfig(SCAFFOLD_HOME, cfg)
+
+    elif command == "getmetrics":
+        ran_command = True
+        fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriverrc.json")
+        fdServer = fossdriverSetup(fossdriverrc_path)
+        if not fdServer:
+            print(f"Unable to connect to Fossology server with fossdriver")
+            sys.exit(1)
+        all_metrics = getMetrics(cfg, fdServer)
+
+        metricsFilename = os.path.join(cfg._storepath, cfg._month, "metrics.json")
+        saveMetrics(metricsFilename, all_metrics)
+
+    elif command == "printmetrics":
+        ran_command = True
+        metricsFilename = os.path.join(cfg._storepath, cfg._month, "metrics.json")
+        printMetrics(metricsFilename)
+
+    elif command == "transfer":
+        ran_command = True
+
+        # set up fossdriver server connections
+        old_fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriverrc.json")
+        old_server = fossdriverSetup(old_fossdriverrc_path)
+        if not old_server:
+            print(f"Unable to connect to old Fossology server with fossdriver")
+            sys.exit(1)
+        new_fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriver3rc.json")
+        new_server = fossdriverSetup(new_fossdriverrc_path)
+        if not new_server:
+            print(f"Unable to connect to new Fossology server with fossdriver")
+            sys.exit(1)
+
+        # run transfer
+        doTransfer(SCAFFOLD_HOME, cfg, prj_only, old_server, new_server)
+        
+    return ran_command
+
 if __name__ == "__main__":
     # check and parse year-month
     if len(sys.argv) < 2:
@@ -99,6 +271,7 @@ if __name__ == "__main__":
         sys.exit(1)
     year, month = datefuncs.parseYM(sys.argv[1])
     if year == 0 and month == 0:
+        print("Invalid month\n")
         printUsage()
         sys.exit(1)
 
@@ -108,142 +281,24 @@ if __name__ == "__main__":
         SCAFFOLD_HOME = os.path.join(Path.home(), "scaffold")
     MONTH_DIR = os.path.join(SCAFFOLD_HOME, datefuncs.getYMStr(year, month))
 
-    ran_command = False
-
-    # load configuration file for this month
-    cfg_file = os.path.join(MONTH_DIR, "config.json")
-    cfg = loadConfig(cfg_file, SCAFFOLD_HOME)
-
-    # we'll check if added optional args limit to one prj / sp
-    prj_only = ""
-    sp_only = ""
-
-    if len(sys.argv) >= 3:
-        month = sys.argv[1]
-        command = sys.argv[2]
-        if len(sys.argv) >= 4:
-            prj_only = sys.argv[3]
-            if len(sys.argv) >= 5:
-                sp_only = sys.argv[4]
-
-        if command == "status":
-            ran_command = True
-            status(cfg, prj_only, sp_only)
-
-        elif command == "newmonth":
-            ran_command = True
-            copyToNextMonth(SCAFFOLD_HOME, cfg)
-
-        elif command == "run":
-            ran_command = True
-            saveBackupConfig(SCAFFOLD_HOME, cfg)
-
-            # set up fossdriver server connection
-            fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriverrc.json")
-            fdServer = fossdriverSetup(fossdriverrc_path)
-            if not fdServer:
-                print(f"Unable to connect to Fossology server with fossdriver")
-                sys.exit(1)
-
-            # run commands
-            doNextThing(SCAFFOLD_HOME, cfg, fdServer, prj_only, sp_only)
-
-            # save modified config file
-            saveConfig(SCAFFOLD_HOME, cfg)
-
-        elif command == "ws":
-            ran_command = True
-            if prj_only == "" or sp_only == "":
-                print(f"ws command requires specifying project and subproject")
-                sys.exit(1)
-
-            # run WS agent manually if between ZIPPEDCODE and CLEARED state
-            # does not modify the config file
-            runManualWSAgent(cfg, prj_only, sp_only)
-
-        elif command == "clear":
-            ran_command = True
-            saveBackupConfig(SCAFFOLD_HOME, cfg)
-
-            # clear if in RANAGENTS state
-            doCleared(SCAFFOLD_HOME, cfg, prj_only, sp_only)
-
-            # save config file, even if not modified (b/c saved backup)
-            saveConfig(SCAFFOLD_HOME, cfg)
-
-        elif command == "approve":
-            ran_command = True
-            saveBackupConfig(SCAFFOLD_HOME, cfg)
-
-            # approve if in MADEDRAFTFINDINGS state
-            doApprove(SCAFFOLD_HOME, cfg, prj_only, sp_only)
-
-            # save config file, even if not modified (b/c saved backup)
-            saveConfig(SCAFFOLD_HOME, cfg)
-
-        elif command == "printemail":
-            ran_command = True
-
-            # print email details if in UPLOADEDREPORTS state
-            printEmail(cfg, prj_only, sp_only)
-
-        elif command == "printlinks":
-            ran_command = True
-
-            # print report link details if in UPLOADEDREPORTS state
-            printAllLinks(cfg, prj_only, sp_only)
-
-        elif command == "printreportlinks":
-            ran_command = True
-
-            # print report link details if in UPLOADEDREPORTS state
-            printReportLinks(cfg, prj_only, sp_only)
-
-        elif command == "deliver":
-            ran_command = True
-            saveBackupConfig(SCAFFOLD_HOME, cfg)
-
-            # clear if in UPLOADEDSPDX state
-            doDelivered(SCAFFOLD_HOME, cfg, prj_only, sp_only)
-
-            # save config file, even if not modified (b/c saved backup)
-            saveConfig(SCAFFOLD_HOME, cfg)
-
-        elif command == "getmetrics":
-            ran_command = True
-            fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriverrc.json")
-            fdServer = fossdriverSetup(fossdriverrc_path)
-            if not fdServer:
-                print(f"Unable to connect to Fossology server with fossdriver")
-                sys.exit(1)
-            all_metrics = getMetrics(cfg, fdServer)
-
-            metricsFilename = os.path.join(cfg._storepath, cfg._month, "metrics.json")
-            saveMetrics(metricsFilename, all_metrics)
-
-        elif command == "printmetrics":
-            ran_command = True
-            metricsFilename = os.path.join(cfg._storepath, cfg._month, "metrics.json")
-            printMetrics(metricsFilename)
-
-        elif command == "transfer":
-            ran_command = True
-
-            # set up fossdriver server connections
-            old_fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriverrc.json")
-            old_server = fossdriverSetup(old_fossdriverrc_path)
-            if not old_server:
-                print(f"Unable to connect to old Fossology server with fossdriver")
-                sys.exit(1)
-            new_fossdriverrc_path = os.path.join(str(Path.home()), ".fossdriver", "fossdriver3rc.json")
-            new_server = fossdriverSetup(new_fossdriverrc_path)
-            if not new_server:
-                print(f"Unable to connect to new Fossology server with fossdriver")
-                sys.exit(1)
-
-            # run transfer
-            doTransfer(SCAFFOLD_HOME, cfg, prj_only, old_server, new_server)
-
-    if ran_command == False:
-        printUsage()
-        sys.exit(1)
+    if sys.argv[2] == "clearlock":
+        clear_lock(MONTH_DIR)
+    else:
+        # load configuration file for this month
+        cfg_file = os.path.join(MONTH_DIR, "config.json")
+        cfg = loadConfig(cfg_file, SCAFFOLD_HOME)
+        ran_command = False
+        if lockfile(MONTH_DIR):
+            try:
+                ran_command = exec_command(SCAFFOLD_HOME, cfg, sys.argv)
+            finally:
+                unlockfile(MONTH_DIR)
+        else:
+            print("""
+It looks like Scaffold is already running for this month.
+If you are Absolutely sure scaffold is Not being run by another user,
+you can run the 'clearlock' command to remove the lock file.
+            """)
+        if not ran_command:
+            printUsage()
+            sys.exit(1) 
