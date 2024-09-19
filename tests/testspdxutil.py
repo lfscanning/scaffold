2390784f936203b4dcc544464d3cd91d969e486a
Gary O'Neall <gary@sourceauditor.com>
2024-09-18 19:57:12 -0700

    Intermediate checkin for Trivy augmentation
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- tests/testspdxutil.py
+++ tests/testspdxutil.py
@@ -1,15 +1,24 @@
+# Copyright The Linux Foundation
+# SPDX-License-Identifier: Apache-2.0
+
 import unittest
 import os
 import tempfile
 import shutil
 import spdx.spdxutil as spdxutil
 import spdx.xlsx as xlsx
+from config import loadConfig
 import pdb
 
 TRIVY_SPDX_FILENAME = "test-trivy-spdx.json"
 TEST_TRIVY_SPDX_PATH = os.path.join(os.path.dirname(__file__), "testresources", TRIVY_SPDX_FILENAME)
 LARGE_SPDX_FILENAME = "cncf-3-keycloak-spdx.json"
 TEST_LARGE_SPDX_PATH = os.path.join(os.path.dirname(__file__), "testresources", LARGE_SPDX_FILENAME)
+MATERIALX_CONFIG_FILENAME = "test-materialx-conf.json"
+MATERIALX_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "testresources", MATERIALX_CONFIG_FILENAME)
+MATERIALX_TRIVY_FILENAME = "MaterialX-2024-08-21-trivy-spdx.json"
+MATERIALX_TRIVY_PATH = os.path.join(os.path.dirname(__file__), "testresources", MATERIALX_TRIVY_FILENAME)
+SECRET_FILE_NAME = ".test-scaffold-secrets.json"
 
 TEST_PACKAGES = {
     "pom.xml" : {
@@ -90,6 +99,16 @@ class TestSpdxUtil(unittest.TestCase):
         shutil.copy(TEST_TRIVY_SPDX_PATH, self.trivy_json_path)
         self.large_json_path = os.path.join(self.temp_dir.name, LARGE_SPDX_FILENAME)
         shutil.copy(TEST_LARGE_SPDX_PATH, self.large_json_path)
+        self.materialx_trivy_path = os.path.join(self.temp_dir.name, MATERIALX_TRIVY_FILENAME)
+        shutil.copy(MATERIALX_TRIVY_PATH, self.materialx_trivy_path)
+        self.scaffold_home_dir = os.path.join(self.temp_dir.name, "scaffold")
+        os.mkdir(self.scaffold_home_dir)
+        os.mkdir(os.path.join(self.scaffold_home_dir, 'spdxrepos'))
+        self.config_dir = os.path.join(self.scaffold_home_dir, '2024-08')
+        os.mkdir(self.config_dir)
+        self.materialx_config_path = os.path.join(self.config_dir, MATERIALX_CONFIG_FILENAME)
+        shutil.copy(MATERIALX_CONFIG_PATH, self.materialx_config_path)
+        
 
     def tearDown(self):
         self.temp_dir.cleanup()
@@ -116,6 +135,30 @@ class TestSpdxUtil(unittest.TestCase):
         # xlsxpath = os.path.join(self.temp_dir.name, "sample_report.xlsx")
         # xlsx.saveXlsx(workbook, xlsxpath)
         # pdb.set_trace()
+        
+    def testFixTrivyDocument(self):
+        spdxDoc = spdxutil.parseFile(self.materialx_trivy_path)
+        cfg = loadConfig(self.materialx_config_path, self.scaffold_home_dir, SECRET_FILE_NAME)
+        prj = cfg._projects['aswf']
+        sp = prj._subprojects['materialx']
+        spdxutil.augmentTrivyDocument(spdxDoc, cfg, prj, sp)
+        # get describes
+        # describes.name == 'awsf.materialx'
+        # str(describes.declared_license) == '(BSD-3-Clause AND BSD-3-Clause AND CC-BY-3.0 AND BSD-3-Clause AND CC-BY-4.0 AND CC-BY-3.0+) OR (MIT AND CC-BY-4.0 AND AMPAS AND Apache-2.0 AND Apache-2.0 WITH LLVM-exception AND Apache-2.0 AND CC-BY-4.0 AND CC-BY-4.0 AND CC-BY-4.0 AND MPL-2.0 AND MPL-2.0 AND CC-BY-4.0 AND MPL-2.0 AND DreamWorks-BSD-style-1)'
+        # str(describes.concluded_license) == '(BSD-3-Clause AND BSD-3-Clause AND CC-BY-3.0 AND BSD-3-Clause AND CC-BY-4.0 AND CC-BY-3.0+) OR (MIT AND CC-BY-4.0 AND AMPAS AND Apache-2.0 AND Apache-2.0 WITH LLVM-exception AND Apache-2.0 AND CC-BY-4.0 AND CC-BY-4.0 AND CC-BY-4.0 AND MPL-2.0 AND MPL-2.0 AND CC-BY-4.0 AND MPL-2.0 AND DreamWorks-BSD-style-1)'
+        # describes.conclude_license == NOASSERTION
+        # describes.contains includes all the repos
+        # repo 'MaterialX':
+            # declared license 'Apache-2.0 AND Apache-2.0 WITH LLVM-exception AND Apache-2.0 AND CC-BY-4.0'
+            # name 'MaterialX'
+            # download_location 'https://github.com/AcademySoftwareFoundation/MaterialX/archive/153a803c46181319fd782ef8426ff58a2e885d82.zip'
+            # externalPackageRef type ExternalPackageRefCategory.PACKAGE_MANAGER reference_type='purl', locator='pkg:github/AcademySoftwareFoundation/MaterialX@153a803c46181319fd782ef8426ff58a2e885d82'
+            # version '153a803c46181319fd782ef8426ff58a2e885d82'
+            # supplier 'Organization: aswf'
+            # primary_package_purpose = PackagePurpose.SOURCE
+            # Fixed up the newly created package should have a CONTAINS relationship relationship_type=<RelationshipType.CONTAINS: 6>, related_spdx_element_id='SPDXRef-Application-95ffd6aeac0971a4'
+            
+            
         
 if __name__ == '__main__':
     unittest.main()
